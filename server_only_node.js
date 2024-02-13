e188bfd023d9b70299ccdc7f2172545c1949a17d
Junior <junior.silva_costa@live.com>
2024-02-13 16:09:55 -0300

    API Node com Postgres

---
+++ server_only_node.js
@@ -0,0 +1,12 @@
+// Aqui utilizamos o node puro sem frameworks
+
+// import { createServer } from 'node:http';
+
+// const server = createServer((request, response) => {
+//     console.log('rodando server');
+
+//     response.write('Hello World!');
+//     return response.end();
+// });
+
+// server.listen(3333);
